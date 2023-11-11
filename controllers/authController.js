08b963100dc12fba32340be29b50e7c2350ef474
pasiuta <pasuta.dima@gmail.com>
2023-11-11 09:56:32 +0200

    extend auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,3 +14,35 @@ exports.registerUser = (req, res) => {
         res.json({ message: 'User registered successfully' });
     });
 };
+
+exports.loginUser = (req, res) => {
+
+    const { username, password } = req.body;
+
+    User.authenticate(username, password, (err, user) => {
+        if (err || !user) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        req.session.userId = user._id;
+
+        res.json({ message: 'Login successful' });
+
+    });
+
+};
+
+exports.logoutUser = (req, res) => {
+
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.json({ message: 'Logged out successfully' });
+        });
+    } else {
+        res.json({ message: 'Not logged in' });
+    }
+
+};
